93018f38e686028b3193a6d845f87e26941a6dfd
Hiroshi Nagata <hirosn@live.com>
2014-11-06 09:03:11 -0800

    Change queue to Queue.

--- test/queue_test.js
+++ test/queue_test.js
@@ -1,51 +1,51 @@
 'use strict';
 
 var expect = require('chai').expect;
-var queue = require('../lib/queue');
+var Queue = require('../lib/queue');
 
 describe('queue', function() {
   it('returns undefined as back of empty queue', function() {
-    expect(queue().back()).equals(undefined);
+    expect(new Queue().back()).equals(undefined);
   });
 
   it('returns zero as the size of empty queue', function() {
-    expect(queue().size()).equals(0);
+    expect(new Queue().size()).equals(0);
   });
 
   it('can push an element and return the size of a queue', function() {
-    expect(queue().
+    expect(new Queue().
       push('foo').
       size()).equals(1);
   });
 
   it('returns the element at the back of a queue', function() {
-    expect(queue().
+    expect(new Queue().
       push('foo').
       push('bar').
       back()).equals('bar');
   });
 
   it('returns true for empty queue', function() {
-    expect(queue().empty()).equals(true);
+    expect(new Queue().empty()).equals(true);
   });
 
   it('returns false for non-empty queue', function() {
-    expect(queue().push(1).empty()).equals(false);
+    expect(new Queue().push(1).empty()).equals(false);
   });
 
   it('returns undefined as front of empty queue', function() {
-    expect(queue().front()).equals(undefined);
+    expect(new Queue().front()).equals(undefined);
   });
 
   it('returns the element at the front of a queue', function() {
-    expect(queue().
+    expect(new Queue().
       push('foo').
       push('bar').
       front()).equals('foo');
   });
 
   it('can pop the front of a queue', function() {
-    expect(queue().
+    expect(new Queue().
       push('foo').
       push('bar').
       pop().
@@ -53,7 +53,7 @@ describe('queue', function() {
   });
 
   it('returns the size after push/pop elements', function() {
-    expect(queue().
+    expect(new Queue().
       push('foo').
       push('bar').
       pop().
@@ -61,14 +61,14 @@ describe('queue', function() {
   });
 
   it('returns string representation of a queue', function() {
-    expect(queue().
+    expect(new Queue().
       push('foo').
       push('bar').
       toString()).equals('foo, bar');
   });
 
   it('returns string representation of empty queue', function() {
-    expect(queue().toString()).to.be.a('string');
-    expect(queue().toString()).equals('');
+    expect(new Queue().toString()).to.be.a('string');
+    expect(new Queue().toString()).equals('');
   });
 });
